c54d65429be9e72709f4601edf46890ea498695d
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-04 08:33:37 +0100

    New file CloneRepo.ps1

---
+++ CloneRepo.ps1
@@ -0,0 +1,18 @@
+﻿# Find repo
+Start-Process https://github.com/www42
+
+# ---------------------------------------
+# For git use PowerShell console, not ISE
+# ---------------------------------------
+
+# Configure git
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+git config --global http.proxy http://192.168.254.5:8080
+git config --list
+
+
+# Clone repo
+mkdir c:\git
+cd C:\git
+git clone  https://github.com/www42/GTTHeidelberg.git
