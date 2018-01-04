368c13141488a9c0acb275e376a0aba55e62b38b
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-04 08:42:39 +0100

    New File

---
+++ ScopeVariable.ps1
@@ -0,0 +1,10 @@
+﻿# Mandatory parameter
+function MyEcho { param( [Parameter(Mandatory=$true)]$text ) echo $text }
+
+# Optional parameter - default defined inside function
+function MyEcho { param ($text = "Kraft durch Shell.") echo $text }
+
+
+# Optional parameter - default defined outside function
+$text = "Wilhelm Leibniz"
+function MyEcho { param ($text = $Global:text) echo $text }
