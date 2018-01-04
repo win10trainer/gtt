51de5b995d7952481bda24175e70ef3f81785ba6
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-04 09:01:51 +0100

    New file

---
+++ PUTT__PsRepository.ps1
@@ -0,0 +1,9 @@
+﻿$RepoName = "MyGet"
+$SourceLocation = 'https://www.myget.org/F/tj/api/v2'
+ 
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
+ 
+Register-PSRepository -Name $RepoName `
+                      -SourceLocation $SourceLocation `
+                      -InstallationPolicy Trusted `
+                      -PackageManagementProvider "NuGet"
